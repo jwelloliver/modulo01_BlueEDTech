4f3ac09b57d28841ad3dd1c32d71c3115d474dcc
Wellington Oliveira <jwell.oliveira@gmail.com>
2022-01-25 21:42:21 -0300

    atualizar exercicio2 da aula 07

--- Aula_07/exercicio2.js
+++ Aula_07/exercicio2.js
@@ -1,7 +1,9 @@
 var prompt = require('prompt-sync')();
 
 let quantNota = +prompt("Digite a quantidade de Notas a serem Aplicadas: ");
-let valornotas = 0;
+let notasAprovadas = 0;
+let notasReprovados = 0;
+let notasRecuperacao = 0;
 const listaNotas = [];
 let cont = 0;
 
@@ -11,9 +13,16 @@ while (cont < quantNota) {
 }
 for (let cont = 0; cont < listaNotas.length; cont++) {
     if (listaNotas[cont] >= 7) {
-   valornotas = valornotas + 1;
-   console.log('Quantidade de alunos Aprovados: %d',valornotas);   
-        
+   notasAprovadas = notasAprovadas + 1;        
     }
-    
+    if (listaNotas[cont] > 5 && listaNotas[cont] > 7) {
+        notasRecuperacao = notasRecuperacao + 1;        
+        }
+    if (listaNotas[cont] < 5) {
+        notasReprovados = notasReprovados + 1;
+        } 
+             
 }
+console.log('Quantidade de alunos Aprovados: %d',notasAprovadas); 
+console.log('Quantidade de alunos Recuperação: %d',notasRecuperacao);
+console.log('Quantidade de alunos Reprovados: %d',notasReprovados);
