4a436f59bdac68310bd751aa539fc7d546994c1e
Wellington Oliveira <jwell.oliveira@gmail.com>
2022-01-27 22:14:46 -0300

    atualizar exercicios extra da aula 09

---
+++ Aula_09/exercicioextra1.js
@@ -0,0 +1,9 @@
+var prompt = require('prompt-sync')();
+
+let cont = 0;
+
+for (let i = 0; i < 10; i++) {
+    cont = cont + 1;
+    console.log(cont);
+}
+
---
+++ Aula_09/exercicioextra2.js
@@ -0,0 +1,9 @@
+var prompt = require('prompt-sync')();
+
+let mult = 1;
+
+
+for (let i = 1; i <= 10; i++) {
+    mult = 8 * i;
+    console.log(`8 x ${i} = ${mult}`);
+}
