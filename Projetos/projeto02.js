caceb7c1673044066c8e337a0a9cd0be918cb345
Wellington Oliveira <jwell.oliveira@gmail.com>
2022-01-30 00:02:44 -0300

    atualizar o projeto 02

--- Projetos/projeto02.js
+++ Projetos/projeto02.js
@@ -19,6 +19,8 @@ console.log("\n ##### Jogo Jokenpô ##### \n ")
 
 let rodadas = +prompt('Digite numero de rodadas: ');
 let cont = 0;
+let vitoriaMinha = 0;
+let vitoriaPc = 0 ;
 
 // Criando um lista de pedra, papel e tesoura
 
@@ -34,21 +36,35 @@ const pcEscolha = listaJogo[Math.floor(Math.random() * listaJogo.length)];
 console.log(` Minha Decisao: ${minhaEscolha}`);
 console.log(` Decisao do compudator: ${pcEscolha}`);
 
-// Condições para cada um Ganhar ou Perder 
+// Condições para cada um Ganhar ou Perder e acumulando a vitoria de cada um na rodadas
 if (pcEscolha == 'PEDRA' && minhaEscolha == 'TESOURA') {    
-    console.log("Computador Ganhou !!");    
+    console.log("Computador Ganhou !!");
+    vitoriaPc = vitoriaPc + 1;    
 }if (minhaEscolha == 'PEDRA' && pcEscolha == 'TESOURA') {
-    console.log("Voce Ganhou !!");    
+    console.log("Voce Ganhou !!");
+    vitoriaMinha = vitoriaMinha + 1;     
 }if (pcEscolha == 'PEDRA' && minhaEscolha == 'PAPEL') {
-    console.log("Voce Ganhou !!"); 
+    console.log("Voce Ganhou !!");
+    vitoriaMinha = vitoriaMinha + 1;  
 }if (minhaEscolha == 'PEDRA' && pcEscolha == 'PAPEL') {
     console.log("Computador Ganhou !!");
+    vitoriaPc = vitoriaPc + 1;
 }if (minhaEscolha == 'TESOURA' && pcEscolha == 'PAPEL') {
-    console.log("Voce Ganhou !!"); 
+    console.log("Voce Ganhou !!");
+    vitoriaMinha = vitoriaMinha + 1;  
 }if (pcEscolha == 'TESOURA' && minhaEscolha == 'PAPEL') {
     console.log("Computador Ganhou !!");
+    vitoriaPc = vitoriaPc + 1;
 }
 
- 
-     
+// Condição fora do While para Esclarecer o vencedor por quantidade de Rodadas vencidas
+if (vitoriaMinha > vitoriaPc) {
+    console.log(`Você venceu com total de vitoria: ${vitoriaMinha}`);
+    console.log(`computador perdeu com total de vitoria: ${vitoriaPc}`);    
+}else{
+    console.log(`O computador venceu com total de vitoria: ${vitoriaPc}`);
+    console.log(`Você perdeu com total de vitoria: ${vitoriaMinha}`);
+}
+
+
 
